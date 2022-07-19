923ede75358cb8c44bb2155a4d100b57cba5bcbc
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-07-19 05:49:21 +0300

    Create filter_len.pas

---
+++ filter_len.pas
@@ -0,0 +1,18 @@
+program FilterLength;
+var
+    n: integer;
+    c: char;
+begin
+    n := 0;
+    while not eof do
+        begin
+            read(c);
+            if c = #10 then
+            begin
+                writeln(n);
+                n := 0
+            end
+            else
+                n := n + 1
+        end
+end.
